ef1aa57c860dd4ce729dea2887673c23d73fe668
laloa554 <laloa554@gmail.com>
2024-05-07 16:56:31 -0600

    initial commit

---
+++ src/test/java/com/WalletLogin/WalletLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WalletLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
